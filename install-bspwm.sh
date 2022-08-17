db62672bef097dcaab1e78c96c35abb5760da5c0
arega24 <99811425+arega24@users.noreply.github.com>
2022-08-17 19:55:15 +0100

    Add files via upload

---
+++ install-bspwm.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Update
+sudo pacman -Syu
+
+# Install paru
+sudo pacman -S git base-devel 
+git clone https://aur.archlinux.org/paru.git
+cd paru 
+makepakg -si
+cd
+
+# xorg
+sudo paru -S xorg-server xorg-xinit xorg-xrandr xorg-backlight
+
+# Video drivers
+
+# ly
+paru -S ly
+sudo systemctl enable ly.service
+
+# bspwm
+sudo paru -S bspwm sxhkd rofi polybar picom python-pywal kitty dunst playerctl pamixer pulsemixer qpwgraph zip tar bat mediainfo ueberzeg trash-cliredshift-minimal rofi-calc 
+
+chmod u+x ~/.config/bspwm/bspwmrc
+
+# fonts
+paru -S ttf-jetbrains-mono ttf-babelstone-runic 
+
+# useful programs
+sudp paru -S vim neofetch htop libqalculate 
+paru -S lf flameshot-git visual-studio-code-bin
+
+
+
