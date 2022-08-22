321a62aa2f69bacd837c2c75034f66771d9c41ba
arega24 <99811425+arega24@users.noreply.github.com>
2022-08-22 13:47:44 +0000

    install-apps

---
+++ install-apps.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo paru -S firefox piper lutris obs-studio
+sudo paru -Sy steam
+paru -S visual-studio-code-bin onlyoffice fluent-reader
+
+flatpak install flathub org.standardnotes.standardnotes
+flatpak install flathub com.todoist.Todoist
+flatpak install flathub com.spotify.Client
+flatpak install flathub com.discordapp.Discord
+flatpak install flathub com.github.micahflee.torbrowser-launcher
