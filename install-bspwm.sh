ab02d5ac4a9d9183e9f916fc046b2633e748c7a9
arega24 <99811425+arega24@users.noreply.github.com>
2022-08-22 17:54:19 +0100

    Update install-bspwm.sh

--- install-bspwm.sh
+++ install-bspwm.sh
@@ -19,12 +19,13 @@ mkdir ~/Imagens
 mkdir ~/Documentos
 mkdir ~/Transferncias
 
-# xorg
+# xorg (it conflicts with nvidia)
 # sudo paru -S xorg-server xorg-xinit xorg-xrandr xorg-backlight xsecurelock xorg-xev
 
 # nvidia propriatery drivers(steam dont work with nouveau)
 wget https://us.download.nvidia.com/XFree86/Linux-x86_64/515.65.01/NVIDIA-Linux-x86_64-515.65.01.run
 # then run this script as sudo 
+# sudo pacman -S --needed nvidia-dkms nvidia-utils lib32-nvidia-utils nvidia-settings vulkan-icd-loader lib32-vulkan-icd-loader
 
 # Video drivers
 # nouveau
