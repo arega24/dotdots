467b4212dc1f7f0a6664c5c0ea4a4f9a9d648078
arega24 <99811425+arega24@users.noreply.github.com>
2022-11-22 18:19:11 +0000

    Update install-apps.sh

--- install-apps.sh
+++ install-apps.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-sudo paru -S firefox qutebrowser piper lutris obs-studio syncthing
+sudo paru -S firefox piper lutris obs-studio syncthing spicetify-cli
 sudo paru -Sy steam
 paru -S code onlyoffice tor-browser keepassxc webapp-manager
 paru -S qutebrowser python-adblock
 
 flatpak install flathub org.standardnotes.standardnotes
-# flatpak install flathub com.todoist.Todoist
-# flatpak install flathub com.spotify.Client
 flatpak install flathub com.discordapp.Discord
 flatpak install flathub com.github.tchx84.Flatseal
 flatpak install flathub com.logseq.Logseq
